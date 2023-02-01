a933a9abe1855c92ce7eab78ba5a85506f9fb107
Giada Dall'Ara <giada.25700@gmail.com>
2023-02-01 17:59:57 +0100

    Esercizio 1 sulle enumerazioni

---
+++ Esercizio/src/Start.java
@@ -0,0 +1,14 @@
+import java.time.Month;
+public class Start02 {
+    enum month {January, February, March, April, May, June, July, August, September, October, November, December};
+    public static void main(String[] args) {
+        for (month mese: month.values()) {
+            String support = mese.toString();
+            if(support.contains("y")){
+                System.out.println(mese + " ends with y");
+            }else {
+                System.out.println(mese + " doesn't end with y");
+            }
+        }
+    }
+}
